f335772bafcedbd94de7694edc20ce0e12335b59
William Ndirangu <wwagura0@gmail.com>
2024-03-24 15:22:21 +0300

    Spelling corrections and additional comments

--- Speed-Detector/index.js
+++ Speed-Detector/index.js
@@ -1,16 +1,17 @@
-// Creates a function that takes in the speed of the vehicle as an argument and then evaluates if the car was speeding or not and then deduct points from the driver //
+// Creates a function that takes in the speed of the vehicle and then evaluates if the car was speeding or not then gives the driver demerit points according to their speed //
 function calculateDemeritPoints() {
-    let speed = parseInt(document.getElementById("speed").value);
-    let demeritPoints = 0;
-
-    if(speed > 70) {
-        demeritPoints = Math.floor((speed - 70) / 5);
-    }
-
-    if(demeritPoints > 12) {
-        document.getElementById("demerit-points").innerText = "Demerit Points: " + demeritPoints + ", License Suspended!";
-    }
-    else {
-        document.getElementById("demerit-points").innerText = "Demerit Points: " + demeritPoints;
-    }
+  let speed = parseInt(document.getElementById("speed").value);
+  let demeritPoints = 0;
+// Checks the input value of the speed to chech if its more than the speed limit and calculate the demerit points //
+  if (speed > 70) {
+    demeritPoints = Math.floor((speed - 70) / 5);
+  }
+// Checks whether the demerit points are more than 12 and output the value "License Suspended!" //
+  if (demeritPoints > 12) {
+    document.getElementById("demerit-points").innerText =
+      "Demerit Points: " + demeritPoints + ", License Suspended!";
+  } else {
+    document.getElementById("demerit-points").innerText =
+      "Demerit Points: " + demeritPoints;
+  }
 }
--- Student Grader/index.js
+++ Student Grader/index.js
@@ -1,29 +1,26 @@
 //Creteas a function to calculate the average score of the student and output the grade of the student//
+// Calculation of the sum of the inputted values and finds the average //
 function calculateGrade() {
-    let score1 = parseInt(document.getElementById("score1").value);
-    let score2 =parseInt(document.getElementById("score2").value);
-    let score3 =parseInt(document.getElementById("score3").value);
-    let score4 =parseInt(document.getElementById("score4").value);
+  let score1 = parseInt(document.getElementById("score1").value);
+  let score2 = parseInt(document.getElementById("score2").value);
+  let score3 = parseInt(document.getElementById("score3").value);
+  let score4 = parseInt(document.getElementById("score4").value);
 
-    let totalScore = score1 + score2 + score3 + score4;
-    let average = totalScore / 4;
-
-    let grade;
-    if(average >= 80) {
-        grade = "A"
-    }
-    else if(average >= 60) {
-        grade = "B"
-    }
-    else if(average >= 50) {
-        grade = "C"
-    }
-    else if(average >= 40)  [
-        grade = "D"
-    ]
-    else {
-        grade = "E"
-    }
-
-    document.getElementById("results").innerText = "Average Score: " + average.toFixed(2) + ", Grade: " + grade;
+  let totalScore = score1 + score2 + score3 + score4;
+  let average = totalScore / 4;
+// Assignment of grades depending on the values of the average //
+  let grade;
+  if (average >= 80) {
+    grade = "A";
+  } else if (average >= 60) {
+    grade = "B";
+  } else if (average >= 50) {
+    grade = "C";
+  } else if (average >= 40) [(grade = "D")];
+  else {
+    grade = "E";
+  }
+// Ouputs the values of the grades //
+  document.getElementById("results").innerText =
+    "Average Score: " + average.toFixed(2) + ", Grade: " + grade;
 }
